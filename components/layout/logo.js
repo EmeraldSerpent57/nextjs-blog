70b1744155572eb3edf668f96a2b9555420291ed
Alaina Harrison <amvoce1221@gmail.com>
2023-08-04 19:53:36 -0700

    changed blog logo

--- components/layout/logo.js
+++ components/layout/logo.js
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./logo.module.css";
 
 function Logo() {
-  return <div className={classes.logo}>Alaina's Next Blog</div>;
+  return <div className={classes.logo}>My Awesome Blog</div>;
 }
 
 export default Logo;
